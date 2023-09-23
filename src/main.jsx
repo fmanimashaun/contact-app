f8e8c98b7cf70507061085fa1acbe029e30aadb0
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-09-23 16:54:08 +0100

    updated route setup

--- src/main.jsx
+++ src/main.jsx
@@ -14,40 +14,45 @@ import editAction from './utils/editAction';
 import deleteAction from './utils/deleteAction';
 import ErrorPage from './routes/ErrorPage';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      loader: rootLoader,
+      action: rootAction,
+      children: [
+        {
+          errorElement: <ErrorPage />,
+          children: [
+            { index: true, element: <Home /> },
+            {
+              path: 'contacts/:contactId',
+              element: <Contact />,
+              loader: contactLoader,
+              action: contactAction,
+            },
+            {
+              path: 'contacts/:contactId/edit',
+              element: <EditContact />,
+              loader: contactLoader,
+              action: editAction,
+            },
+            {
+              path: 'contacts/:contactId/delete',
+              action: deleteAction,
+              errorElement: <div>Oops! There was an error.</div>,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Home /> },
-          {
-            path: 'contacts/:contactId',
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: 'contacts/:contactId/edit',
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: 'contacts/:contactId/delete',
-            action: deleteAction,
-            errorElement: <div>Oops! There was an error.</div>,
-          },
-        ],
-      },
-    ],
+    basename: '/contact-app',
   },
-]);
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
